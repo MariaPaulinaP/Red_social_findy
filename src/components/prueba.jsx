83540554b294571b7440dc017af03864718ee134
Maria Paulina Parra <mariapaulinap0531@gmail.com>
2023-08-11 10:19:32 -0500

    Creacion de proyecto en react red social

---
+++ src/components/prueba.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Prueba() {
+  return (
+    <h1>Prueba</h1>
+  )
+}
+
+export default Prueba
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Prueba from './components/prueba'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+
+  <>
+  <Prueba/>
+  </>
+)
