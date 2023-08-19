c479a750f599c4518c14424ca11260ce27891cec
Maria Paulina Parra <mariapaulinap0531@gmail.com>
2023-08-19 09:41:44 -0500

    formulario inicio sesion

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Prueba from './components/prueba'
+import Router from './routers/Router'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-
   <>
-  <Prueba/>
+  <Router/>
   </>
 )
---
+++ src/routers/Router.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useReducer, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { initialUser, userReducer } from "../service/admiReducer/admiReducer";
+import { createContext } from "react";
+import Form from '../components/form/Form'
+import { getSession } from "../service/sessionService/sessionService";
+
+export const AppContext = createContext({});
+
+const Router = () => {
+    const [nameUser, setNameUser] = useState([]);
+  useEffect(() => {
+    const user = getSession();
+    if (user?.name) {
+      userDispatch({
+        type: "login",
+        payload: {
+          isAutenticated: true,
+          user: user,
+        },
+      });
+    }
+  }, []);
+
+    const [userLogin, userDispatch] = useReducer(userReducer, initialUser);
+    const globalState = {
+        user: {
+        userLogin,
+        userDispatch,
+        },
+    };
+
+  return (
+    <AppContext.Provider value={globalState}>
+        <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<Form/>}/>
+                </Routes>
+        </BrowserRouter>
+   </AppContext.Provider>
+  )
+}
+
+export default Router
+
